58ccf071a8bfa289e4c3531ccd0c077897f92d60
SabaPangani <spangani37@gmail.com>
2024-05-27 00:00:07 +0400

    starter

---
+++ src/Navbar.tsx
@@ -0,0 +1,18 @@
+import ArrowDownSvg from "./components/svgs/ArrowDownSvg";
+
+export default function Navbar() {
+  return (
+    <nav className="px-10 w-[1180px] mx-auto my-7 flex flex-row justify-between items-center z-10 absolute left-1/2 -translate-x-1/2 top-0 text-white">
+      <h1 className="text-xl font-medium">Logo</h1>
+
+      <ul className="flex flex-row items-center gap-x-10 font-medium cursor-pointer">
+        <li>Home</li>
+        <li className="flex flex-row items-center gap-x-2">Services <ArrowDownSvg /></li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+
+      <h1 className="font-medium">+995 599 410821</h1>
+    </nav>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./routes/root";
+import "./index.css";
+import Home from "./routes/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [{ path: "home", element: <Home /> }],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
