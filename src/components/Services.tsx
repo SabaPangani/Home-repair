8bde8d6592b643e62d7cc2076c6efb0371693d34
SabaPangani <spangani37@gmail.com>
2024-06-04 14:44:06 +0400

    services

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,90 +1,162 @@
+import HomeSvg from "./svgs/HomeSvg";
+import building from "../components/svgs/home.png";
+import painting from "../components/svgs/paint-palette.png";
+import installation from "../components/svgs/installation.png";
+import sofa from "../components/svgs/sofa.png";
+import planning from "../components/svgs/planning.png";
+import sketch from "../components/svgs/sketch.png";
 export default function Services() {
   return (
-    <div className="flex flex-col items-center justify-center mx-auto bg-[#f2f2f2]">
-      <div className="flex flex-row items-center justify-between my-10 w-[1380px]">
-        <div className="flex flex-col uppercase">
-          <h1 className="text-[60px] text-[#9caf88] font-medium h-16">
-            Our Home{" "}
-          </h1>
-          <h1 className="text-[60px] text-[#6d7a5f] font-medium h-16">
-            Repair Services
-          </h1>
-        </div>
-        <p className="w-full max-w-[500px] text-lg font-light">
-          At Apex Windows Werks, we specialize in repairing all types of windows
-          – big or small, interior or exterior. We’ll get your broken windows
-          fixed quickly and efficiently.
+    // <div className="flex flex-col items-center justify-center mx-auto bg-[#f2f2f2]">
+    //   <div className="flex flex-row items-center justify-between my-10 w-[1380px]">
+    //     <div className="flex flex-col uppercase">
+    //       <h1 className="text-[60px] text-[#9caf88] font-medium h-16">
+    //         Our Home{" "}
+    //       </h1>
+    //       <h1 className="text-[60px] text-[#6d7a5f] font-medium h-16">
+    //         Repair Services
+    //       </h1>
+    //     </div>
+    //     <p className="w-full max-w-[500px] text-lg font-light">
+    //       At Apex Windows Werks, we specialize in repairing all types of windows
+    //       – big or small, interior or exterior. We’ll get your broken windows
+    //       fixed quickly and efficiently.
+    //     </p>
+    //   </div>
+
+    //   <div className="flex flex-row gap-x-10 my-10 w-[1380px] justify-between">
+    //     <div className="w-full max-w-[393px] h-[520px] rounded-lg border-2 flex justify-center items-center flex-col hover:scale-105 transition-all cursor-pointer">
+    //       <img
+    //         src="https://apexwindowwerks.com/app/uploads/2023/03/IMG_4535-768x512.webp"
+    //         alt=""
+    //         className="rounded-lg rounded-b-none h-[300px] shadow-xl"
+    //       />
+    //       {/* <div className="w-[353px] h-1/3 absolute top-[150px] bg-custom-gradient"></div> */}
+    //       <div className="bg-[#9caf8819] px-3 h-full pt-3 rounded-b-lg">
+    //         <h1 className="text-[30px] text-[#9caf88] uppercase font-medium">
+    //           Window Maintenance
+    //         </h1>
+    //         <p className="font-light">
+    //           Whether replacing a broken window or repairing a damaged one, our
+    //           team's skill and know-how can tackle any challenge, no matter how
+    //           complex. With years of experience and many successful projects,
+    //           we're a reliable partner you can count on for efficiency,
+    //           professional craftsmanship, and exceptional customer service.
+    //         </p>
+    //       </div>
+    //     </div>
+    //     <div className="w-full max-w-[393px] h-[520px] rounded-lg border-2 flex justify-center items-center flex-col hover:scale-105 transition-all cursor-pointer">
+    //       <img
+    //         src="https://apexwindowwerks.com/app/uploads/2023/03/IMG_4535-768x512.webp"
+    //         alt=""
+    //         className="rounded-lg rounded-b-none h-[300px] shadow-xl"
+    //       />
+    //       {/* <div className="w-[353px] h-1/3 absolute top-[150px] bg-custom-gradient"></div> */}
+    //       <div className="bg-[#9caf8819] px-3 h-full pt-3 rounded-b-lg">
+    //         <h1 className="text-[30px] text-[#9caf88] uppercase font-medium">
+    //           Window Maintenance
+    //         </h1>
+    //         <p className="font-light">
+    //           Whether replacing a broken window or repairing a damaged one, our
+    //           team's skill and know-how can tackle any challenge, no matter how
+    //           complex. With years of experience and many successful projects,
+    //           we're a reliable partner you can count on for efficiency,
+    //           professional craftsmanship, and exceptional customer service.
+    //         </p>
+    //       </div>
+    //     </div>
+    //     <div className="w-full max-w-[393px] h-[520px] rounded-lg border-2 flex justify-center items-center flex-col hover:scale-105 transition-all cursor-pointer">
+    //       <img
+    //         src="https://apexwindowwerks.com/app/uploads/2023/03/IMG_4535-768x512.webp"
+    //         alt=""
+    //         className="rounded-lg rounded-b-none h-[300px] shadow-xl"
+    //       />
+    //       {/* <div className="w-[353px] h-1/3 absolute top-[150px] bg-custom-gradient"></div> */}
+    //       <div className="bg-[#9caf8819] px-3 h-full pt-3 rounded-b-lg">
+    //         <h1 className="text-[30px] text-[#9caf88] uppercase font-medium">
+    //           Window Maintenance
+    //         </h1>
+    //         <p className="font-light">
+    //           Whether replacing a broken window or repairing a damaged one, our
+    //           team's skill and know-how can tackle any challenge, no matter how
+    //           complex. With years of experience and many successful projects,
+    //           we're a reliable partner you can count on for efficiency,
+    //           professional craftsmanship, and exceptional customer service.
+    //         </p>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="flex justify-betweeen gap-x-20 items-center w-[1380px] mx-auto my-10">
+      <div className="bg-[#f8f8f8] min-h-[400px] w-full max-w-[300px] p-[50px]">
+        <p className="text-[#797979] text-sm">OUR SERVICES</p>
+        <h1 className="text-[40px] text-[#000000] font-medium">SERVICES </h1>
+        <p className="mb-3 text-[#797979] font-light">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+          reprehenderit corrupti commodi iusto eum quam culpa odio itaque
+          explicabo doloribus, sint magnam aliquid alias? Odio.
+        </p>
+        <p className="text-[#797979] font-light">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati,
+          quod sequi alias magnam maxime ut excepturi nemo, odio incidunt,
+          sapiente nam accusamus. Mollitia, fugit itaque.
         </p>
       </div>
+      <ul className="flex flex-row flex-wrap gap-5 justify-end text-center gap-y-10">
+        <li className="flex justify-center flex-col items-center bg-[#f8f8f8] py-10 px-5 gap-y-2 w-[300px]">
+          <img src={painting} alt="" className="w-14 h-14" />
+          <h2 className="text-[20px] opacity-90">PAINTING</h2>
+          <p className="text-[#797979] text-sm">
+            Lorem ipsum dolor sit amet consectetur consectetur
+          </p>
+        </li>
+        <li className="flex justify-center flex-col items-center bg-[#f8f8f8] py-10 px-5 gap-y-2 w-full max-w-[300px]">
+          <img src={installation} alt="" className="w-14 h-14" />
+          <h2 className="text-[20px] opacity-90">INSTALLING</h2>
+          <p className="text-[#797979] text-sm">
+            Lorem ipsum dolor sit amet consectetur consectetur
+          </p>
+        </li>
+        <li className="flex justify-center flex-col items-center bg-[#f8f8f8] py-10 px-5 gap-y-2 w-full max-w-[300px]">
+          <img
+            src={building}
+            alt=""
+            className="w-14 h-14"
+          />
+          <h2 className="text-[20px] opacity-90">BUILDING</h2>
+          <p className="text-[#797979] text-sm">
+            Lorem ipsum dolor sit amet consectetur consectetur
+          </p>
+        </li>
+        <li className="flex justify-center flex-col items-center bg-[#f8f8f8] py-10 px-5 gap-y-2 w-full max-w-[300px]">
+          <img src={sketch} alt="" className="w-14 h-14" />
+          <h2 className="text-[20px] opacity-90">DESIGN</h2>
+          <p className="text-[#797979] text-sm">
+            Lorem ipsum dolor sit amet consectetur consectetur
+          </p>
+        </li>
+        <li className="flex justify-center flex-col items-center bg-[#f8f8f8] py-10 px-5 gap-y-2 w-full max-w-[300px]">
+          <img src={planning} alt="" className="w-14 h-14" />
+          <h2 className="text-[20px] opacity-90">PLANNING</h2>
+          <p className="text-[#797979] text-sm">
+            Lorem ipsum dolor sit amet consectetur consectetur
+          </p>
+        </li>
+        <li className="flex justify-center flex-col items-center bg-[#f8f8f8] py-10 px-5 gap-y-2 w-full max-w-[300px]">
+          <img src={sofa} alt="" className="w-14 h-14" />
 
-      <div className="flex flex-row gap-x-10 my-10 w-[1380px] justify-between">
-        <div className="w-full max-w-[393px] h-[520px] rounded-lg border-2 flex justify-center items-center flex-col hover:scale-105 transition-all cursor-pointer">
-          <img
-            src="https://apexwindowwerks.com/app/uploads/2023/03/IMG_4535-768x512.webp"
-            alt=""
-            className="rounded-lg rounded-b-none h-[300px] shadow-xl"
-          />
-          {/* <div className="w-[353px] h-1/3 absolute top-[150px] bg-custom-gradient"></div> */}
-          <div className="bg-[#9caf8819] px-3 h-full pt-3 rounded-b-lg">
-            <h1 className="text-[30px] text-[#9caf88] uppercase font-medium">
-              Window Maintenance
-            </h1>
-            <p className="font-light">
-              Whether replacing a broken window or repairing a damaged one, our
-              team's skill and know-how can tackle any challenge, no matter how
-              complex. With years of experience and many successful projects,
-              we're a reliable partner you can count on for efficiency,
-              professional craftsmanship, and exceptional customer service.
-            </p>
-          </div>
-        </div>
-        <div className="w-full max-w-[393px] h-[520px] rounded-lg border-2 flex justify-center items-center flex-col hover:scale-105 transition-all cursor-pointer">
-          <img
-            src="https://apexwindowwerks.com/app/uploads/2023/03/IMG_4535-768x512.webp"
-            alt=""
-            className="rounded-lg rounded-b-none h-[300px] shadow-xl"
-          />
-          {/* <div className="w-[353px] h-1/3 absolute top-[150px] bg-custom-gradient"></div> */}
-          <div className="bg-[#9caf8819] px-3 h-full pt-3 rounded-b-lg">
-            <h1 className="text-[30px] text-[#9caf88] uppercase font-medium">
-              Window Maintenance
-            </h1>
-            <p className="font-light">
-              Whether replacing a broken window or repairing a damaged one, our
-              team's skill and know-how can tackle any challenge, no matter how
-              complex. With years of experience and many successful projects,
-              we're a reliable partner you can count on for efficiency,
-              professional craftsmanship, and exceptional customer service.
-            </p>
-          </div>
-        </div>
-        <div className="w-full max-w-[393px] h-[520px] rounded-lg border-2 flex justify-center items-center flex-col hover:scale-105 transition-all cursor-pointer">
-          <img
-            src="https://apexwindowwerks.com/app/uploads/2023/03/IMG_4535-768x512.webp"
-            alt=""
-            className="rounded-lg rounded-b-none h-[300px] shadow-xl"
-          />
-          {/* <div className="w-[353px] h-1/3 absolute top-[150px] bg-custom-gradient"></div> */}
-          <div className="bg-[#9caf8819] px-3 h-full pt-3 rounded-b-lg">
-            <h1 className="text-[30px] text-[#9caf88] uppercase font-medium">
-              Window Maintenance
-            </h1>
-            <p className="font-light">
-              Whether replacing a broken window or repairing a damaged one, our
-              team's skill and know-how can tackle any challenge, no matter how
-              complex. With years of experience and many successful projects,
-              we're a reliable partner you can count on for efficiency,
-              professional craftsmanship, and exceptional customer service.
-            </p>
-          </div>
-        </div>
-      </div>
+          <h2 className="text-[20px] opacity-90">FURNISHINGS</h2>
+          <p className="text-[#797979] text-sm">
+            Lorem ipsum dolor sit amet consectetur consectetur
+          </p>
+        </li>
+      </ul>
     </div>
   );
 }
 
-
-        {/* <div className="w-full max-w-[393px] py-3 px-6 rounded-md border-2  flex justify-center items-center flex-col hover:scale-105 transition-all cursor-pointer">
+{
+  /* <div className="w-full max-w-[393px] py-3 px-6 rounded-md border-2  flex justify-center items-center flex-col hover:scale-105 transition-all cursor-pointer">
           <img
             src="https://apexwindowwerks.com/app/uploads/2023/05/IMG_4148.webp"
             alt=""
@@ -100,4 +172,5 @@ export default function Services() {
             we're a reliable partner you can count on for efficiency,
             professional craftsmanship, and exceptional customer service.
           </p>
-        </div> */}
+        </div> */
+}
---
+++ src/components/svgs/HomeSvg.tsx
@@ -0,0 +1,25 @@
+export default function HomeSvg() {
+  return (
+    <div>
+      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          {" "}
+          <path
+            d="M4 21H10M14 21H20M4 17.5H10M14 17.5H20M4 14H10M14 14H20M19 8.44444V5M12 7.00671L12.0074 6.99998M12 3L21 10H3L12 3Z"
+            stroke="#000000"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            data-darkreader-inline-stroke=""
+          ></path>{" "}
+        </g>
+      </svg>
+    </div>
+  );
+}
